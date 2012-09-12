df73552dc137c5f2f7e9d9ee5a09d6b0c0d1ae42
Daniel Leitner <d4l.l5r@googlemail.com>
2012-09-12 17:04:28 +0200

    Initial commit.

---
+++ apps/things/main.js
@@ -0,0 +1,30 @@
+// ==========================================================================
+// Project:   Things
+// Copyright: @2012 My Company, Inc.
+// ==========================================================================
+/*globals Things */
+
+// This is the function that will start your app running.  The default
+// implementation will load any fixtures you have created then instantiate
+// your controllers and awake the elements on your page.
+//
+// As you develop your application you will probably want to override this.
+// See comments for some pointers on what to do next.
+//
+Things.main = function main() {
+
+  // Step 1: Instantiate Your Views
+  // The default code here will make the mainPane for your application visible
+  // on screen.  If you app gets any level of complexity, you will probably 
+  // create multiple pages and panes.  
+  Things.getPath('mainPage.mainPane').append() ;
+
+  // Step 2. Set the content property on your primary controller.
+  // This will make your app come alive!
+
+  // TODO: Set the content property on your primary controller
+  // ex: Things.contactsController.set('content',Things.contacts);
+
+} ;
+
+function main() { Things.main(); }
