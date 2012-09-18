36518f5f4806597a79177f2370b09a38430a7a35
Daniel Leitner <d4l.l5r@googlemail.com>
2012-09-18 20:15:37 +0200

    Add sortable and Bootstrap support.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
-var App = Em.Application.create();
+/**var App = Em.Application.create();
 
 App.MyView = Em.View.extend({
   mouseDown: function() {
     window.alert("hello world!");
   }
+});**/
+
+$(document).ready(function() {
+	// Inititlize Bootstrap Plugins
+	$('a[rel=tooltip]').tooltip();
+	$('#new').modal().modal('hide');
 });
